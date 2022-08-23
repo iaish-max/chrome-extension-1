5a400dd4512866ef576d6ed1b9c5066fe70daf81
iaish-max <aishwarybhadauriya09@gmail.com>
2022-08-23 17:51:04 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const changeBackgroundColor = () => {
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "red";
+  };
+
+  function test() {
+    /* eslint-disable no-undef */
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTabId = tabs[0].id;
+      chrome.scripting.executeScript({
+        target: { tabId: activeTabId },
+        function: changeBackgroundColor,
+      });
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={test}>Send Alert</button>
     </div>
   );
 }
